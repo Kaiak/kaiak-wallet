d60a350310b8a2b963726a490a796fc64cc894db
Kjetil Mehl <kjetil@mehl.no>
2020-11-25 17:43:15 +0100

    Adds dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/js/app.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
     },
 };
