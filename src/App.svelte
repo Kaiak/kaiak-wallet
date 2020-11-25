3c26fc79c59088a717f15af97798e5f2e9d9419f
Kjetil Mehl <kjetil@mehl.no>
2020-11-25 21:39:41 +0100

    Adds more of original

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
 <script>
 	import Receive from "./svelte/view/Receive.svelte";
+
+	let header = "Receive"
 </script>
 
+
 <main>
-	<Receive />
+<div id="kui-app">
+	<div class="kui-header">
+		<h1 class="kui-h1" id="kui-header">{header}</h1>
+	</div>
+	<div class="kui-content-area" id="content-area">
+		<Receive />
+		<div id="kui-options" class="kui-option-menu"></div>
+	</div>
+</div>
+<div class="kui-software-key">
+	<h5 role="button" class="kui-h5 kui-text-left" id="kui-left-soft-key"></h5>
+	<h5 role="button" class="kui-h5 kui-text-center kui-text-upcase" id="kui-middle-soft-key"></h5>
+	<h5 role="button" class="kui-h5 kui-text-right">Menu</h5>
+</div>
 </main>
