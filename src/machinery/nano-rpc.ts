99b004e6cb5670b1257a53da77fee3a8ed40df29
Kjetil Mehl <kjetil@mehl.no>
2020-11-28 18:41:10 +0100

    Fixed library, removes ts-ignore

--- src/machinery/nano-rpc.ts
+++ src/machinery/nano-rpc.ts
@@ -9,7 +9,6 @@ const nanoApi = new NodeRPCsApi(new Configuration({
 
 export async function resolveHistory(address: string): Promise<NanoTransaction[]> {
     let history: AccountHistoryResponse = await nanoApi.accountHistory({
-        // @ts-ignore TODO: Fix the library exporting these wrong
         accountHistoryRequest: {
             action: AccountHistoryRequestActionEnum.AccountHistory,
             account: address,
