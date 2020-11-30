717b5bdec10f0b9463fbe7669e7a524ab9a052c6
Philipp Blum <philipp-blum@jakiku.de>
2020-11-30 17:18:43 +0100

    Fix ts style with ESLint

--- src/constants/views.ts
+++ src/constants/views.ts
@@ -1,39 +1,39 @@
 export interface View {
-    viewKey: string
-    title: string
+  viewKey: string;
+  title: string;
 }
 
 export const SETUP_VIEW: View = {
-    viewKey: "SETUP_VIEW",
-    title: "Setup",
-}
+  viewKey: 'SETUP_VIEW',
+  title: 'Setup',
+};
 
 export const RECEIVE_VIEW = {
-    viewKey: "RECEIVE_VIEW",
-    title: "Receive"
-}
+  viewKey: 'RECEIVE_VIEW',
+  title: 'Receive',
+};
 
 export const MENU_VIEW = {
-    viewKey: "MENU_VIEW",
-    title: "Menu"
-}
+  viewKey: 'MENU_VIEW',
+  title: 'Menu',
+};
 export const BALANCE_VIEW = {
-    viewKey: "BALANCE_VIEW",
-    title: "Balance",
+  viewKey: 'BALANCE_VIEW',
+  title: 'Balance',
 };
 export const TRANSACTIONS_VIEW = {
-    viewKey: "TRANSACTIONS_VIEW",
-    title: "Transactions",
+  viewKey: 'TRANSACTIONS_VIEW',
+  title: 'Transactions',
 };
 export const SEND_VIEW = {
-    viewKey: "SEND_VIEW",
-    title: "Send"
-}
+  viewKey: 'SEND_VIEW',
+  title: 'Send',
+};
 export const ABOUT_VIEW = {
-    viewKey: "ABOUT_VIEW",
-    title: "About"
-}
+  viewKey: 'ABOUT_VIEW',
+  title: 'About',
+};
 export const BACK = {
-    viewKey: "BACK",
-    title: undefined,
-}
+  viewKey: 'BACK',
+  title: undefined,
+};
--- src/machinery/models.ts
+++ src/machinery/models.ts
@@ -1,23 +1,23 @@
-export type NanoAddress = string
-export type Seed = string
+export type NanoAddress = string;
+export type Seed = string;
 
 export interface NanoAccount {
-    alias: string
-    address: NanoAddress
+  alias: string;
+  address: NanoAddress;
 }
 
 export interface NanoWallet {
-    accounts: NanoAccount[]
-    seed: Seed
+  accounts: NanoAccount[];
+  seed: Seed;
 }
 
 export interface NanoTransaction {
-    account: NanoAddress
-    amount: string
-    type: string
-    localTimestamp: string
+  account: NanoAddress;
+  amount: string;
+  type: string;
+  localTimestamp: string;
 }
 
 export interface Balance {
-    amount: string
+  amount: string;
 }
