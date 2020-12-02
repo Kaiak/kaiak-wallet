d913f69e45640e833e37b39980fd82cb5bd5c0e0
Kjetil Mehl <kjetil@mehl.no>
2020-12-02 20:13:11 +0100

    Removes view

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -1,12 +1,7 @@
 import { Writable, writable } from 'svelte/store';
-import { WALLET_VIEW } from '../constants/views';
-import type { View } from '../constants/views';
 import type { LoadedElements } from '../machinery/eventListener';
-import type { NanoAccount } from '../machinery/models';
-import type { AccountAction } from '../constants/account-actions';
 import type {NavigationState} from "../machinery/NavigationState";
 
-export const viewStore: Writable<View> = writable<View>(WALLET_VIEW);
 export const loadedComponentStore: Writable<LoadedElements> = writable<LoadedElements>(
   {
     elements: [],
