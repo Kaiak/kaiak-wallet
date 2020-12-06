8776020c7f479b3812df458b878a94962089707b
Kjetil Mehl <kjetil@mehl.no>
2020-12-06 18:34:15 +0100

    Moves software keys to file

---
+++ src/view/SoftwareKeys.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import {pushMenu} from "../machinery/eventListener";
+
+    const toggleMenu = () => pushMenu('menu')
+</script>
+
+<div class="kui-software-key">
+    <h5 role="button" class="kui-h5 kui-text-left" id="kui-left-soft-key"></h5>
+    <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase" id="kui-middle-soft-key"></h5>
+    <h5 role="button" class="kui-h5 kui-text-right" on:click={toggleMenu} data-l10n-id="rightNavButton"></h5>
+</div>
