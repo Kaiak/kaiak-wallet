b5f821c54092173603866a327f39a014aec6fdd9
Kjetil Mehl <kjetil@mehl.no>
2020-12-07 23:11:45 +0100

    Makes it possible to change alias of account

---
+++ src/view/wallet/Settings.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type {NanoAccount} from "../../machinery/models";
+    import Seperator from "../../components/Seperator.svelte";
+    import LabelledInput from "../../components/LabelledInput.svelte";
+    import Button from "../../components/Button.svelte";
+
+    export let account: NanoAccount;
+    export let storeFunction: () => void
+
+    let aliasValue: string = account?.alias
+
+    const setAlias = (event) => {
+        console.log(account)
+        aliasValue = event.target.value;
+    }
+
+    const save = async () => {
+        account.alias = aliasValue
+        storeFunction()
+    }
+
+</script>
+
+<Seperator languageId="account-settings" />
+<LabelledInput languageId="account-alias" on:change={setAlias} bind:value={aliasValue}></LabelledInput>
+<Button languageId="save-button" on:click={save}></Button>
