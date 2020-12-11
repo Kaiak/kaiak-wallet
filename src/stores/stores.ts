11ffdb5eac2a74585886dfbb6c7d8d967a7f94d7
Philipp Blum <philipp-blum@jakiku.de>
2020-12-11 21:45:35 +0100

    Fix merge conflict in stores

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -2,11 +2,8 @@ import { Writable, writable } from 'svelte/store';
 import type { LoadedElements } from '../machinery/eventListener';
 import type { NavigationState } from '../machinery/NavigationState';
 import type { SoftwareKeysState } from '../machinery/SoftwareKeysState';
-<<<<<<< HEAD
-import type { ToastState } from '../machinery/ToastState';
-=======
 import { START_STATE } from '../machinery/NavigationState';
->>>>>>> org/master
+import type { ToastState } from '../machinery/ToastState';
 
 export const loadedComponentStore: Writable<LoadedElements> = writable<LoadedElements>(
   {
