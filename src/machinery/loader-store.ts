9edf915cae693cdaf2230abe79d0060a105c35b6
Kjetil Mehl <kjetil@mehl.no>
2021-01-03 17:38:48 +0100

    Simplifies a bit

--- src/machinery/loader-store.ts
+++ src/machinery/loader-store.ts
@@ -9,17 +9,16 @@ export interface Loader {
 export const loaderStore = writable<Loader | undefined>(undefined);
 
 export async function load(loader: Loader) {
-  try {
-    loaderStore.set(loader);
-    setTimeout(
-      async () =>
-        await loader
-          .load()
-          .catch(() => loader.onError?.())
-          .finally(() => loaderStore.set(undefined)),
-      50 // TODO: HACK to get loading screen to show
-    );
-  } catch (error) {
-    console.log(`error loading: ${error}`);
-  }
+  loaderStore.set(loader);
+  setTimeout(
+    async () =>
+      await loader
+        .load()
+        .then(() => loaderStore.set(undefined))
+        .catch(() => {
+          loader.onError?.();
+          loaderStore.set(undefined);
+        }),
+    50 // TODO: HACK to get loading screen to show
+  );
 }
