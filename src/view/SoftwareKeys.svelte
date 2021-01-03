13adddf34534db0d402609783b12996b1c23052b
Kjetil Mehl <kjetil@mehl.no>
2021-01-03 09:41:35 +0100

    Moves menu css only to menu component

--- src/view/SoftwareKeys.svelte
+++ src/view/SoftwareKeys.svelte
@@ -32,6 +32,24 @@
     .kui-software-key h5 {
         width: 33.3333333333%;
     }
+    .kui-h5 {
+        font-size: 14px;
+        font-weight: 600;
+    }
+    .kui-text-left {
+        text-align: left;
+    }
+
+    .kui-text-center {
+        text-align: center;
+    }
+
+    .kui-text-right {
+        text-align: right;
+    }
+    .kui-text-upcase {
+        text-transform: uppercase;
+    }
 </style>
 
 <div class="kui-software-key">
@@ -39,7 +57,7 @@
     {#if leftKey}
         <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase" on:click={leftKey.onClick} data-l10n-id={leftKey.languageId}></h5>
     {:else}
-        <h5 role="button" class="kui-h5 kui-text-left" id="kui-left-soft-key"></h5>
+        <h5 role="button" class="kui-h5 kui-text-left kui-text-upcase" id="kui-left-soft-key"></h5>
     {/if}
     {#if middleKey}
         <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase" on:click={middleKey.onClick} data-l10n-id={middleKey.languageId}></h5>
@@ -47,8 +65,8 @@
         <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase"></h5>
     {/if}
     {#if rightKey}
-        <h5 role="button" class="kui-h5 kui-text-right" on:click={rightKey.onClick} data-l10n-id={rightKey.languageId}></h5>
+        <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase" on:click={rightKey.onClick} data-l10n-id={rightKey.languageId}></h5>
     {:else}
-        <h5 role="button" class="kui-h5 kui-text-right"></h5>
+        <h5 role="button" class="kui-h5 kui-text-center kui-text-upcase"></h5>
     {/if}
 </div>
