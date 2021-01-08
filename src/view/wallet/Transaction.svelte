8fea2ce28f9dfbf2a589aa9f52eb8cbaac9577be
Kjetil Mehl <kjetil@mehl.no>
2021-01-09 00:28:39 +0100

    Prepare set address

--- src/view/wallet/Transaction.svelte
+++ src/view/wallet/Transaction.svelte
@@ -2,13 +2,24 @@
     import type {NanoTransaction} from "../../machinery/models";
     import Seperator from "../../components/Seperator.svelte";
     import Text from "../../components/Text.svelte";
-    import {rawToNano} from "../../machinery/nanocurrency-web-wrapper";
     import {transactionText} from "../../machinery/text-utils";
-    import Primary from "../../components/list/Primary.svelte";
     import {transactionTime} from "../../machinery/text-utils";
     import {transactionReceived} from "../../machinery/text-utils";
+    import {afterUpdate} from "svelte";
+    import {navigationReload, pushAccountAction} from "../../machinery/eventListener";
 
     export let transaction: NanoTransaction
+
+    afterUpdate(() => {
+        navigationReload({
+            leftKey: {
+                languageId: 'send-button',
+                onClick: async () => {
+                    pushAccountAction('send_address')
+                }
+            }
+        })
+    })
 </script>
 <Seperator languageId={transactionReceived(transaction)} />
 <Text>{transactionTime(transaction)}</Text>
