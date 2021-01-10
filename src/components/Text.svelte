091e05f7dbd432a81253139741490223cba8c907
Kjetil Mehl <kjetil@mehl.no>
2021-01-10 01:23:55 +0100

    Adds more texts

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -1,17 +1,21 @@
-<script>
+<script lang="ts">
     export let languageId;
     export let breakAll;
+    export let bold: boolean = false;
 </script>
 
 <style>
     div:focus {
         outline: none;
     }
+    .bold {
+        font-weight: bold;
+    }
 </style>
 
 {#if breakAll}
-    <div class="kui-body kui-text kui-text-break navigation" tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></div>
+    <div class="kui-body kui-text kui-text-break navigation" class:bold tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></div>
 {:else}
-    <div class="kui-body kui-text navigation" tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></div>
+    <div class="kui-body kui-text navigation" class:bold tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></div>
 {/if}
 
