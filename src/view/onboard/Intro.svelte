caeca6e8aac27610a456baf1c1aec931bc192795
Kjetil Mehl <kjetil@mehl.no>
2021-01-10 13:34:24 +0100

    Moves to intro file, can use onmount

---
+++ src/view/onboard/Intro.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Seperator from "../../components/Seperator.svelte";
+    import Text from "../../components/Text.svelte";
+    import {onMount} from "svelte";
+    import {navigationReload, pushOnboardState} from "../../machinery/eventListener";
+
+    onMount(() => navigationReload({
+        leftKey: {
+            languageId: 'create',
+            onClick: async () => {
+                pushOnboardState({view: 'disclaimer', walletResult: undefined, alias: undefined})
+            }
+        },
+        rightKey: {
+            onClick: async () => pushOnboardState({view: 'disclaimer-import'}),
+            languageId: 'import-wallet'
+        }
+    }))
+</script>
+
+<Text bold={true} languageId="title" />
+<Seperator languageId="what-is-nano-title"/>
+<Text languageId="onboard-title" />
+<Seperator languageId="onboard-import-or-create"/>
+<Text breakAll={false} languageId="onboard-description"/>
+
