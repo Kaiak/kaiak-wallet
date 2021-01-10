3aea411e0dd695491a3f8307670846d208db7062
Kjetil Mehl <kjetil@mehl.no>
2021-01-10 16:17:31 +0100

    Adds title as Kaios spec

--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     export let languageId;
     export let breakAll;
-    export let bold: boolean = false;
 </script>
 
 <style>
@@ -11,12 +10,9 @@
     span:focus {
         outline: none;
     }
-    .bold {
-        font-weight: bold;
-    }
     .text-break {
         word-break: break-all;
     }
 </style>
 
-<span class="kui-text navigation" class:text-break={breakAll} class:bold tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></span>
+<span class="kui-text navigation" class:text-break={breakAll} tabindex="1" data-l10n-id="{languageId}" on:click on:focus><slot/></span>
---
+++ src/components/Title.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let languageId: string
+</script>
+
+<style>
+    h2 {
+        font-size: 17px;
+        font-weight: 600;
+    }
+</style>
+
+<h2 data-l10n-id="{languageId}"></h2>
