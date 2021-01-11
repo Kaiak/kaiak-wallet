6195917de5263216e4da6639e8a6134fc1991a23
Kjetil Mehl <kjetil@mehl.no>
2021-01-12 00:04:49 +0100

    Detects existing wallet, gives warning

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let languageId: string
+    export let warning: boolean = false;
 </script>
 
 <style>
@@ -8,6 +9,9 @@
         font-weight: 600;
         padding: 5px;
     }
+    .warning {
+        color: red;
+    }
 </style>
 
-<h2 data-l10n-id="{languageId}"></h2>
+<h2 data-l10n-id="{languageId}" class:warning></h2>
