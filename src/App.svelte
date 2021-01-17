a30262f85b6300b802681824a98510d534b29de7
Kjetil Mehl <kjetil@mehl.no>
2021-01-17 15:51:18 +0100

    We don't need font

--- src/App.svelte
+++ src/App.svelte
@@ -47,8 +47,6 @@
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 	<style>
 		body {
 			font-family: 'Open Sans', sans-serif;
